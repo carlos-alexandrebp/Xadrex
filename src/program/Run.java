5ca2c656b0b30a3184ae3e4dd5474a9d5f53b72f
carlos-alexandrebp <java-util@hotmail.cor.email>
2019-01-29 19:05:19 -0200

    create first class position

--- src/program/Run.java
+++ src/program/Run.java
@@ -1,6 +1,11 @@
 package program;
+
+import boardgame.Position;
+
     public class Run {
         public static void main(String[] args) {
-            System.out.println("Ola Mundo");
+            Position pos = new Position(3, 5);
+            
+            System.out.println(pos);
         }
     }
